85a8cb1e59ac3d67c4005167126bf1b41f949e9d
maortizv <maortizv@falabella.cl>
2023-08-07 19:37:06 -0400

    se agregan nuevas funciones

--- funciones.py
+++ funciones.py
@@ -4,3 +4,11 @@ def suma(num1, num2):
 
 def resta(num1, num2):
     return num1 - num2
+
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+
+def division(num1, num2):
+    return num1 / num2
